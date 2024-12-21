325bb04316535b7162bfd9ec05b31130fdb40c82
AsierLoayza <loayza.149352@e.unavarra.es>
2024-12-21 17:39:28 +0100

    clase cliente

---
+++ UpnaDirect/src/upnadirect/Cliente.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package upnadirect;
+
+/**
+ *
+ * @author alumno
+ */
+public class Cliente {
+    private int anioNacimiento;
+    private double salario;
+
+    public Cliente(int anioNacimiento, double salario) {
+        this.anioNacimiento = anioNacimiento;
+        this.salario = salario;
+    }
+
+    public int getEdad() {
+        return 2024 - anioNacimiento;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+}
