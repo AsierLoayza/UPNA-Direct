fe11143f571d9a81cfb3a3282b7cbda6bced79ce
AsierLoayza <loayza.149352@e.unavarra.es>
2024-12-21 19:05:28 +0100

    clase bien

---
+++ UpnaDirect/src/upnadirect/Bien.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package upnadirect;
+
+/**
+ *
+ * @author alumno
+ */
+public class Bien {
+  
+    private String tipo; // "vehículo" o "vivienda"
+    private double valor;
+
+    public Bien(String tipo, double valor) {
+        this.tipo = tipo;
+        this.valor = valor;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+}
+
